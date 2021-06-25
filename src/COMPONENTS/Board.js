bfb1a752c77d5d7057dc16f9673a49a97ccc9531
shashi <shashik024@gmail.com>
2021-06-25 18:46:10 +0530

    added board value state and event

--- src/COMPONENTS/Board.js
+++ src/COMPONENTS/Board.js
@@ -1,22 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 function Board() {
+  //useState get initial state as argument
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [isNextx, setisNextx] = useState(true);
+
+  // console.log(board);
+
+  // const handelSquareValue = position => {
+  //   if (board[position]) {
+  //     return;
+  //   }
+  //   setBoard(prevState => {
+  //     return prevState.map((square, pos) => {
+  //       if (pos == position) {
+  //         return isNextx ? 'X' : 'O';
+  //       }
+  //       return square;
+  //     });
+  //   });
+  //   // setBoard(5);
+  //   setisNextx(prevState => !prevState);
+  // };
+
+  // const renderSquare = position => {
+  //   return (
+  //     <Square
+  //       value={board[position]}
+  //       onClick={() => {
+  //         handelSquareValue(position);
+  //       }}
+  //     />
+  //   );
+  // };
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNextx, setNextx] = useState(true);
+
+  const handelSquareValue = position => {
+    if (board[position]) {
+      return;
+    }
+    setBoard(prevState => {
+      return prevState.map((Square, pos) => {
+        if (pos == position) {
+          return isNextx ? 'X' : 'O';
+        }
+        return Square;
+      });
+    });
+
+    setNextx(preState => !preState);
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handelSquareValue(position);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row ">
-        <Square value={0} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row ">
-        <Square value={0} />
-        <Square value={0} />
-        <Square value={0} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row ">
-        <Square value={0} />
-        <Square value={0} />
-        <Square value={0} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/COMPONENTS/Square.js
+++ src/COMPONENTS/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Square = props => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
